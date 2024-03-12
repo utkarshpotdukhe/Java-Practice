dd8aa755d51ee0b9ada838a8278463bba82493a4
Utkarsh Potdukhe <utkarsh16potdukhe@gmail.com>
2024-03-13 00:47:25 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/IncrementAndDecrement/Tutorial1.java
@@ -0,0 +1,10 @@
+package IncrementAndDecrement;
+
+public class Tutorial1 {
+    public static void main(String[] args) {
+        int a,b;
+        a = 30;
+        b = ++a;
+        System.out.println(b);
+    }
+}
---
+++ src/IncrementAndDecrement/Tutorial3.java
@@ -0,0 +1,9 @@
+package IncrementAndDecrement;
+
+public class Tutorial3 {
+    public static void main(String[] args) {
+        int a = 40;
+        a = ++a + 2;
+        System.out.println(a);
+    }
+}
